c115767c570ec193e12024681e78f76e2a452374
sooryakriz11 <sooryakriz111@gmail.com>
2024-12-14 00:05:12 +0530

    style: configure manrope font setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        manrope: ['var(--font-manrope)', 'sans-serif'], 
+      },
     },
   },
   plugins: [],
