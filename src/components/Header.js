69733f7a47b6b304a31f573581b06043d0c97516
sooryakriz11 <sooryakriz111@gmail.com>
2024-12-14 00:33:41 +0530

    style:heading component added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className="p-6 container mx-auto flex items-center justify-between">
+            {/* Logo Image */}
+            <div className="w-[146px] h-[39px]    relative">
+                <Image src={"/pricemaven-logo-ai-product-development.avif"} fill="true" className="object-contain" alt="Pricemaven logo - AI-powered product development tools for accurate project estimation, scoping, and documentation" />
+            </div>
+            {/* Twitter Redirect */}
+            <Link className="" href={""}>
+                <Image src={"/twitter.svg"} width={30} height={30} className="object-contain" alt="Pricemaven logo - AI-powered product development tools for accurate project estimation, scoping, and documentation" />
+            </Link>
+        </header>
+    )
+}
