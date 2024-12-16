55e6b4b55b64d696ea73b17c20f6204df79afb05
sooryakriz11 <sooryakriz111@gmail.com>
2024-12-17 03:54:35 +0530

    style:arrow update

--- src/components/HeroLeft.js
+++ src/components/HeroLeft.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AvatarRow from "./AvatarRow"; // Importing the AvatarRow component to show avatars of subscribed users
 import Input from "./Input"; // Importing a custom Input component for subscription
 import TextAccent from "./TextAccent"; // Custom component for styling accent text
@@ -5,29 +6,42 @@ import TextHeading from "./TextHeading"; // Custom heading component for consist
 
 export default function HeroLeft() {
   return (
-    <div className="flex items-center justify-center flex-col md:items-start"> {/* Centering content on mobile and displaying as block on desktop */}
+    <div className="flex items-center justify-center flex-col md:items-start">
+      {/* Centering content on mobile and displaying as block on desktop */}
       {/* Heading Section */}
       <TextHeading
         text={"Accelerate Your Product Development."} // Main heading text
         accentText={"Build Faster."} // Accent text for emphasis
-        addStyles={"text-[42px] md:text-[35px] lg:text-[48px] font-extrabold text-center md:text-left "} // Custom styles for font size, weight, and alignment
+        addStyles={
+          "text-[42px] md:text-[35px] lg:text-[48px] font-extrabold text-center md:text-left "
+        } // Custom styles for font size, weight, and alignment
       />
       <br />
       {/* Description Paragraph */}
       <p className="text-content-secondary md:text-base lg:text-lg">
-        Scope and estimate your projects accurately, streamline your workflow, 
+        Scope and estimate your projects accurately, streamline your workflow,
         and speed up your product development - all in one place.
-        <TextAccent> Powered by AI</TextAccent> {/* Accent text for highlighting AI */}
+        <TextAccent> Powered by AI</TextAccent>{" "}
+        {/* Accent text for highlighting AI */}
       </p>
       <br />
-      {/* Subscription Input */}
-      <Input
-        btnText={"Join the Waitlist"} // Button text for subscription
-        subText={
-          "By subscribing you're confirming that you agree with our Terms and Conditions." // Subtext with terms
-        }
-      />
-
+      <div className="w-full relative flex items-center justify-center md:justify-start">
+        {/* Subscription Input */}
+        <Input
+          btnText={"Join the Waitlist"} // Button text for subscription
+          subText={
+            "By subscribing you're confirming that you agree with our Terms and Conditions." // Subtext with terms
+          }
+        />
+        <Image
+          src="/arrow.svg"
+          width={75}
+          height={75}
+           className="absolute hidden md:block -right-16 xl:right-10 -top-2"
+          alt="pricemaven hero arrow icon"
+          priority
+        />
+      </div>
       <br />
       {/* Avatar and Subscription Count */}
       <div className="flex flex-col items-center justify-center md:flex-row md:justify-start">
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,15 @@
 export default function Input({ btnText, subText, small }) {
   return (
-    <div>
+    <div className={!small && 'w-full'}>
       {/* Input Container: This div holds the input field and button */}
-      <div className="relative w-full max-w-[520px] md:text-[17px]">
+      <div className="relative w-full md:max-w-[520px] md:text-[17px]">
         
         {/* Input Field: The input field for the user's email, with conditional padding */}
         <input
           type="text"
           className={`placeholder:text-center md:placeholder:text-left pl-6 pr-16 ${
             small ? "py-3" : "py-4" // Conditional padding based on 'small' prop
-          } rounded-full border border-gray-300  md:text-sm lg:text-base focus:outline-none focus:ring-1 focus:ring-primary-light focus:ring-opacity-10 w-full`}
+          } rounded-full border border-gray-300   md:text-sm lg:text-base focus:outline-none focus:ring-1 focus:ring-primary-light focus:ring-opacity-10 w-full`}
           placeholder="Enter your email address"
         />
         
